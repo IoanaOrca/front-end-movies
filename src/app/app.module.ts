031679f8ac44334578a7ce38035ed2ee2c384663
Ioana Iordache <ioanaorka@gmail.com>
2018-05-03 17:13:21 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MovieService } from './services/movie.service';
+import { MovieCardComponent } from './components/movie-card/movie-card.component';
+
+const routes: Routes = [
+  // { path: '', redirectTo: '/movies', pathMatch: 'full' },
+  { path: '',  component: HomePageComponent }
+  // { path: 'movies/:id', component: MovieDetailPageComponent },
+  // { path: 'movies/:create', component: MovieCreatePageComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    MovieCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MovieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
+
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent implements OnInit {
+
+  movies: Array<any>;
+
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit() {
+
+    this.movieService.listAll()
+    .then((data) => {
+      this.movies=data;
+    })
+  }
+
+}
